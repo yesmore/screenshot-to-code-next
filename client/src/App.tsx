1ba04604bcf36f88cb02edcb6b1cba604ae0c20f
yesmore <3224266014@qq.com>
2023-12-24 13:57:19 +0800

    添加demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -35,6 +35,13 @@ import PromptPanel from "./components/PromptPanel";
 import Whiteboard from "./components/Whiteboard";
 import NativePreview from "./components/NativeMobile";
 import { UrlInputSection } from "./components/UrlInputSection";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./components/ui/accordion";
+import { Label } from "./components/ui/label";
 
 const IS_OPENAI_DOWN = false;
 
@@ -387,6 +394,26 @@ function App() {
             </a>
             .
           </div>
+
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="text-slate-500 font-bold text-sm">
+                查看使用示例
+              </AccordionTrigger>
+              <AccordionContent className="space-y-4 flex flex-col">
+                <div className="flex flex-col items-center justify-between">
+                  <Label className="mb-2">草图生成代码</Label>
+                  <video
+                    src="https://img.aoau.top/draw/image-to-code-1.mp4"
+                    controls></video>
+                  <Label className="mt-4 mb-2">截图生成代码</Label>
+                  <video
+                    src="https://img.aoau.top/draw/image-to-code.mp4"
+                    controls></video>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
           {
             <HistoryDisplay
               history={appHistory}
