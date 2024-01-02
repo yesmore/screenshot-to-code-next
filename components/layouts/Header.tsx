7fad08881c040ccbbd1de3ff5847445787eeb71b
yesmore <3224266014@qq.com>
2024-01-02 10:13:18 +0800

    upd title

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -8,7 +8,7 @@ export default () => {
       <div className="bg-white flex p-2 pl-4 border-b-[1px] border-r-[1px] fixed left-0 lg:w-96 z-[49]">
         <img src="/logo.png" width="30" height="30" alt="logo" />
         <h1 className="ml-2 text-2xl font-semibold bg-gradient-to-r from-slate-600 via-slate-500 to-slate-400 bg-clip-text text-transparent">
-          Codify
+          Codify.ICU
         </h1>
       </div>
     </header>
